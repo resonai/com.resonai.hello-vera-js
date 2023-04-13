9af5f04b7c00653320d35477ecaedf2f6e69a18e
Ran Lehr <ran@resonaixr.com>
2023-04-14 02:30:39 +0300

    Following PR comments: - Update vera-sdk dependency to v0.0.9 - Stop using jseComm. - Reduce html content and add comments when it's being updated using JS + warn about navigation being W.I.P. - Remove vacapp.html

--- sample-apps/messaging/main.js
+++ sample-apps/messaging/main.js
@@ -1,55 +1,49 @@
-import { jseApiHelper, jseComm } from '@resonai/vera-sdk'
+import { jseApiHelper } from '@resonai/vera-sdk'
 
-{
-  const packageName = 'com.resonai.ran_test_js'
-  const messagingButtonId = 'hello-vera-js.messaging'
-  
-  async function init() {
-    jseApiHelper.onMessage(onMessage)
-    jseApiHelper.registerButtons({ buttons: [{ id: messagingButtonId }] })
-    jseApiHelper.loaded()
-    document.getElementById('send-button').addEventListener('click', onSendClick)
-    document.getElementById('take-photo-button').addEventListener('click', onTakePhotoClick)
-  }
-  init()
+const packageName = 'com.resonai.ran_test_js'
+const messagingButtonId = 'hello-vera-js.messaging'
 
-  function onSendClick() {
-    const msg = document.getElementById('send-text').value
-    const data = { msg }
-    console.log('sendNativeMessage: ', msg)
-    jseApiHelper.sendNativeMessage({ data })
-    // For simulating in VCC, we'll send it to ourselves
-    jseApiHelper.sendArmeMessage({ packageName, data })
-  }
+async function init() {
+  jseApiHelper.onMessage(onMessage)
+  jseApiHelper.registerButtons({ buttons: [{ id: messagingButtonId }] })
+  jseApiHelper.loaded()
+  document.getElementById('send-button').addEventListener('click', onSendClick)
+  document.getElementById('take-photo-button').addEventListener('click', onTakePhotoClick)
+}
+init()
 
-  function onMessage(data) {
-    console.log('onMessage', data)
-    const textarea = document.getElementById('receive-text')
-    textarea.value += data.msg + '\r\n'
-    textarea.scrollTop = textarea.scrollHeight;
-  }
+function onSendClick() {
+  const msg = document.getElementById('send-text').value
+  const data = { msg }
+  console.log('sendNativeMessage: ', msg)
+  jseApiHelper.sendNativeMessage({ data })
+  // For simulating in VCC, we'll send it to ourselves
+  jseApiHelper.sendArmeMessage({ packageName, data })
+}
 
-  async function onTakePhotoClick() {
-    try {
-      const photo = await jseComm.call('takePhoto', { immediate: true })
-      if (photo.cancelled) {
-        console.log('takePhoto cancelled')
-        return
-      }
-      let imgElem = document.getElementById('img')
-      if (!imgElem) {
-        imgElem = new Image(300);
-        imgElem.id = 'img';
-        imgElem.style.transform = 'rotate(90deg)';
-        imgElem.style.marginTop = '100px'
-        imgElem.style.border = "solid red 5px"
-        document.getElementById('photo-div').appendChild(imgElem)
-      }
-      imgElem.src = 'data:image/png;base64,' + photo
-      console.log('takePhoto succeeded')
+function onMessage(data) {
+  console.log('onMessage', data)
+  const textarea = document.getElementById('receive-text')
+  textarea.value += data.msg + '\r\n'
+  textarea.scrollTop = textarea.scrollHeight;
+}
+
+async function onTakePhotoClick() {
+  try {
+    const photo = await jseApiHelper.takePhoto()
+    let imgElem = document.getElementById('img')
+    if (!imgElem) {
+      imgElem = new Image(300);
+      imgElem.id = 'img';
+      imgElem.style.transform = 'rotate(90deg)';
+      imgElem.style.marginTop = '100px'
+      imgElem.style.border = "solid red 5px"
+      document.getElementById('photo-div').appendChild(imgElem)
     }
-    catch (e) {
-      console.error('takePhoto failed', e)
-    }
+    imgElem.src = 'data:image/png;base64,' + photo
+    console.log('takePhoto succeeded')
+  }
+  catch (e) {
+    console.error('takePhoto failed', e)
   }
 }
--- sample-apps/navigation/main.js
+++ sample-apps/navigation/main.js
@@ -1,63 +1,69 @@
 import { jseApiHelper } from '@resonai/vera-sdk'
 
-{
-  const navigationPackageName = 'com.resonai.navigation'
-  const navigationButtonId = 'hello-vera-js.navigation'
-  let poiSemanticObjectKeys = undefined
-  let siteId = undefined
+const navigationPackageName = 'com.resonai.navigation'
+const navigationButtonId = 'hello-vera-js.navigation'
+let poiSemanticObjectKeys = []
+let siteId = undefined
 
-  async function init() {
-    jseApiHelper.registerButtons({ buttons: [{ id: navigationButtonId }] })
-    jseApiHelper.loaded()
-    const siteConfig = await jseApiHelper.getSiteConfig()
-    siteId = siteConfig.siteId
-    const appConfig = await jseApiHelper.getAppConfig() // this is done too early. When loaded through link - appConfig is not ready.
-    const pointsOfInterest = appConfig['Points of Interest']
-    poiSemanticObjectKeys = pointsOfInterest?.map((poi => poi.key))
-    document.getElementById('navigate-button').addEventListener('click', onNavigateClick)
-    document.getElementById('show-map-button').addEventListener('click', onShowMapClick)
-  }
-  init()
+async function init() {
+  jseApiHelper.registerButtons({ buttons: [{ id: navigationButtonId }] })
+  jseApiHelper.loaded()
+  const siteConfig = await jseApiHelper.getSiteConfig()
+  siteId = siteConfig.siteId
+  const appConfig = await jseApiHelper.getAppConfig() // this is done too early. When loaded through link - appConfig is not ready.
+  const pointsOfInterest = appConfig['Points of Interest']
+  poiSemanticObjectKeys = pointsOfInterest?.map((poi => poi.key)) || []
+  document.getElementById('navigate-deeplink-button').addEventListener('click', onNavigateUsingDeeplinkClick)
+  document.getElementById('navigate-message-button').addEventListener('click', onNavigateUsingMessageClick)
+  document.getElementById('show-map-button').addEventListener('click', onShowMapClick)
+}
+init()
 
-  function onNavigateClick() {
-    if (!poiSemanticObjectKeys || poiSemanticObjectKeys.length === 0) {
-      console.error('Please configure "Points of Interest" and refresh')
-      return
-    }
-    navigateUsingDeeplink()
-    // navigateUsingArmeMessage()
+function onNavigateUsingDeeplinkClick() {
+  if (!isValidSemanticObjectKeys()) {
+    return
   }
+  const data = { key: poiSemanticObjectKeys[0] }
+  const dataJson = JSON.stringify(data)
+  const deepLinkParts = []
+  const prefix = 'https://vera.resonai.com/#/play'
+  deepLinkParts.push(prefix)
+  deepLinkParts.push(encodeURIComponent(siteId))
+  deepLinkParts.push(encodeURIComponent(navigationPackageName))
+  deepLinkParts.push(encodeURIComponent(dataJson))
+  const deepLinkUrl = deepLinkParts.join('/')
+  console.log('openUrl: ', deepLinkUrl)
+  jseApiHelper.openUrl(deepLinkUrl)
+}
 
-  function navigateUsingDeeplink() {
-    const data = { key: poiSemanticObjectKeys[0] }
-    const dataJson = JSON.stringify(data)
-    const deepLinkParts = []
-    const prefix = 'https://vera.resonai.com/#/play'
-    deepLinkParts.push(prefix)
-    deepLinkParts.push(encodeURIComponent(siteId))
-    deepLinkParts.push(encodeURIComponent(navigationPackageName))
-    deepLinkParts.push(encodeURIComponent(dataJson))
-    const deepLinkUrl = deepLinkParts.join('/')
-    jseApiHelper.openUrl(deepLinkUrl)
+function onNavigateUsingMessageClick() {
+  if (!isValidSemanticObjectKeys()) {
+    return
   }
+  const data = {
+    msg: 'navigateTo',
+    poi: poiSemanticObjectKeys[0]
+  }
+  jseApiHelper.sendArmeMessage({ packageName: navigationPackageName, data })
+  alert(data.msg + ' message is not supported yet')
+}
 
-  function navigateUsingArmeMessage() {
-    const data = {
-      msg: 'navigateTo',
-      poi: poiSemanticObjectKeys[0]
-    }
-    jseApiHelper.sendArmeMessage({ navigationPackageName, data })
+function onShowMapClick() {
+  if (!isValidSemanticObjectKeys()) {
+    return
   }
+  const data = {
+    msg: 'showMap',
+    pois: poiSemanticObjectKeys
+  }
+  jseApiHelper.sendArmeMessage({ packageName: navigationPackageName, data })
+  alert(data.msg + ' message is not supported yet')
+}
 
-  function onShowMapClick() {
-    if (!poiSemanticObjectKeys || poiSemanticObjectKeys.length === 0) {
-      console.error('Please configure "Points of Interest" and refresh')
-      return
-    }
-    const data = {
-      msg: 'showMap',
-      pois: poiSemanticObjectKeys
-    }
-    jseApiHelper.sendArmeMessage({ navigationPackageName, data })
+function isValidSemanticObjectKeys() {
+  if (poiSemanticObjectKeys.length === 0) {
+    console.error('Please configure "Points of Interest" and refresh')
+    return false
   }
+  return true
 }
