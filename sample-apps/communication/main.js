b0748c66061f8a21d83126c562c90cda48c2a9a6
orenco-resonai <92263920+orenco-resonai@users.noreply.github.com>
2024-03-17 18:49:49 +0200

    Demonstrate ability to show destination POI only in the navigation preview screen

--- sample-apps/communication/main.js
+++ sample-apps/communication/main.js
@@ -73,16 +73,20 @@ async function init () {
   document.getElementById('navigate-message-noaccessibility-button').addEventListener('click', () => {
     onNavigateUsingMessageClick({ noaccessibility: true })
   })
+  document.getElementById('navigate-message-dest-only-button').addEventListener('click', () => {
+    onNavigateUsingMessageClick({ showDestinationOnly: true })
+  })
   document.getElementById('native-message-button').addEventListener('click', onNativeMessageClick)
   document.getElementById('nearest-poi-button').addEventListener('click', onNearestPoiClick)
 }
 init()
 
-function onNavigateUsingMessageClick ({ skipPreview = false, noaccessibility = false } = {}) {
+function onNavigateUsingMessageClick ({ skipPreview = false, noaccessibility = false, showDestinationOnly = false } = {}) {
   const data = {
     msg: 'navigateTo',
     poi: pointOfInterest.key,
     skipPreview,
+    showDestinationOnly,
     register: true,
     packageName: communicationSamplePackageName,
     actions: { navigationSuccess: true, navigationCancelled: true }
